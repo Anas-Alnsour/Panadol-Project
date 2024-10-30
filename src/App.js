2a0fdfcebf9e210107451591ec9f5872003a9969
Anas-Alnsour <anasalnsour2004@gmail.com>
2024-10-30 13:50:55 +0300

    Updated App component with basename and added custom ErrorPage

--- src/App.js
+++ src/App.js
@@ -7,24 +7,30 @@ import Contact from './Pages/Contact';
 import Service from './Pages/services';
 import Products from './Pages/Product';
 import WhereToBuy from './Pages/buy';
+import ErrorPage from './Pages/ErrorPage'; 
 
-
-const routes = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<Layout/>}>
-    <Route index path='/home' element={<Home/>}/>
-    <Route path='/contact' element={<Contact/>}/>
-    <Route path='/services' element={<Service/>}/>
-    <Route path='/products' element={<Products />} />
-    <Route path='/WhereToBuy' element={<WhereToBuy />} />
-  </Route>
-))
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
+      <Route index path="home" element={<Home />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="services" element={<Service />} />
+      <Route path="products" element={<Products />} />
+      <Route path="WhereToBuy" element={<WhereToBuy />} />
+    </Route>
+  ),
+  {
+    basename: '/Panadol-Project', 
+  }
+);
 
 function App() {
   return (
     <Fragment>
-      <RouterProvider router={routes}/>
+      <RouterProvider router={routes} />
     </Fragment>
   );
 }
 
 export default App;
+
